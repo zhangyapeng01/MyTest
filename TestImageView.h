ba4c09edc4b231bafe8c5664dd7644df7e330c57
zhangyapeng <1365160860@qq.com>
2017-11-27 10:41:45 +0800

    TestImageView

---
+++ TestImageView.h
@@ -0,0 +1,12 @@
+//
+//  TestImageView.h
+//  AFNetworking
+//
+//  Created by zhangyapeng on 2017/11/27.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestImageView : UIView
+
+@end
---
+++ TestImageView.m
@@ -0,0 +1,20 @@
+//
+//  TestImageView.m
+//  AFNetworking
+//
+//  Created by zhangyapeng on 2017/11/27.
+//
+
+#import "TestImageView.h"
+
+@implementation TestImageView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
